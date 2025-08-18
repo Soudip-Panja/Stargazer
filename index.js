4bb0bb8810e1963d01176ef3799758d047a5df7b
Soudip Panja <soudip03panja@gmail.com>
2025-08-18 19:21:58 +0530

    feat: Add single new data in an existing data.

--- index.js
+++ index.js
@@ -32,4 +32,30 @@ async function seedData() {
     console.log("Error seeding the data", error);
   }
 }
-seedData();
+// seedData();
+
+const newMovie = {
+  title: "New Movie",
+  releaseYear: 2023,
+  genre: ["Drama"],
+  director: "Aditya Roy Chopra",
+  actors: ["Actor1", "Actor2"],
+  language: "Hindi",
+  country: "India",
+  rating: 6.1,
+  plot: "A young man and woman fall in love on a Australia trip.",
+  awards: "IFA Filmfare Awards",
+  posterUrl: "https://example.com/new-poster1.jpg",
+  trailerUrl: "https://example.com/new-trailer1.mp4",
+};
+
+async function creeateMovie(newMovie) {
+  try {
+    const movie = new Movie(newMovie);
+    const saveMovie = await movie.save();
+    console.log("New Movie data:", saveMovie);
+  } catch (error) {
+    throw error;
+  }
+}
+creeateMovie(newMovie);
